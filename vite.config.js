80379676407d9f298ed4b2bf2479e0ec2f11147e
Donnie Plumly <donnie.plumly@gmail.com>
2025-10-02 15:23:07 -0700

    netlify

--- vite.config.js
+++ vite.config.js
@@ -1,39 +1,20 @@
-// import { fileURLToPath, URL } from 'node:url'
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-
-// export default defineConfig({
-//   plugins: [vue(), vueDevTools()],
-//   resolve: {
-//     alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
-//   },
-//   server: {
-//     proxy: {
-//       // No rewrite. Frontend calls /api/*, Vite forwards to :3000/api/*
-//       '/api': {
-//         target: 'http://localhost:3000',
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-// })
-
 import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/DF25-DFgives-Serverless/', // ⬅️ add this line
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      },
     },
   },
 })
+
